39beb22f9adbf94e9fd2486b00aee7cdd70b297e
DevC11 <devilcry11@163.com>
2023-09-28 16:43:25 +0800

    js-2

---
+++ js-2/index.js
@@ -0,0 +1,34 @@
+// var num = 1
+// var num2 = 11.2
+// num+=1
+// num2-=1
+// num+="1"
+// // console.log(num,num2)
+
+// var x = 100 / "Apple";
+
+// console.log(typeof x);
+ 
+
+// var y = new Number(2)
+
+// console.log(y);
+
+// var x = 123;
+//  // 从变量 x 返回 123
+// console.log(x.toString());
+// (123).toString();        // 从文本 123 返回 123
+// console.log((100 + 23).toString());
+// (100 + 23).toString();   // 从表达式 100 + 23 返回 123
+
+
+// var a =1.6666
+// // console.log(a.toFixed(2))
+
+// console.log(a.toPrecision(4))
+
+// 1.在JS中八进制前面加0，十六进制前面加 0x
+var num1 = 07;   // 对应十进制的7
+// 2.十六进制数字序列范围：0~9以及A~F
+ var num = 0xA;
+ console.log(num1,num)
